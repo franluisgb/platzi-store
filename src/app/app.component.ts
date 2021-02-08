1d6bc32ea0a758d3358c0852a15ed2bf3ad8f000
grupo_sattis <it@gruposattis.com>
2021-02-07 20:46:36 -0500

    fix(core): proyecto angular platzi-store

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,62 @@
-import { Component, VERSION } from '@angular/core';
+import { Component } from '@angular/core';
+import { Product } from './product.model';
 
 @Component({
-  selector: 'my-app',
+  selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  styleUrls: ['./app.component.scss']
 })
-export class AppComponent  {
-  name = 'Angular ' + VERSION.major;
+export class AppComponent {
+  title = 'platzi-store';
+  items = ['Francisco', 'Eliana', 'Kyra', 'Kissy'];
+  productos: Product[] = [
+    {
+      id: '1',
+      titulo: 'Camiseta',
+      imagen: 'assets/images/camiseta.png',
+      precio: 100000,
+      descripcion: 'Descripcion de la camiseta',
+    },
+    {
+      id: '2',
+      titulo: 'Hoodie',
+      imagen: 'assets/images/hoodie.png',
+      precio: 50000,
+      descripcion: 'Descripcion del hoodie',
+    },
+    {
+      id: '3',
+      titulo: 'Mug',
+      imagen: 'assets/images/mug.png',
+      precio: 25000,
+      descripcion: 'Descripcion del mug',
+    },
+    {
+      id: '4',
+      titulo: 'Pin',
+      imagen: 'assets/images/pin.png',
+      precio: 75000,
+      descripcion: 'Descripcion del pin',
+    },
+    {
+      id: '5',
+      titulo: 'Stickers',
+      imagen: 'assets/images/stickers1.png',
+      precio: 5000,
+      descripcion: 'Descripcion del stickers',
+    },
+    {
+      id: '6',
+      titulo: 'Stickers',
+      imagen: 'assets/images/stickers2.png',
+      precio: 2500,
+      descripcion: 'Descripcion del stickers',
+    },
+  ];
+  addItem() {
+    this.items.push('Nuevo integrante');
+  }
+  deleteItem(index: number) {
+    this.items.splice(index, 1);
+  }
 }
---
+++ src/app/product.model.ts
@@ -0,0 +1,7 @@
+export interface Product {
+    id: string;
+    titulo: string;
+    imagen: string;
+    precio: number;
+    descripcion: string;
+}
